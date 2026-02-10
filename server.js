7b7ce9ddf9703ad6eae985509893a816b4e4c756
Sandile Thabede <thabethesandile44@gmail.com>
2026-02-10 07:18:56 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -16,7 +16,11 @@ const PORT = process.env.PORT || 5000;
 /* ---------------- MIDDLEWARE ---------------- */
 app.use(
   cors({
-    origin: ['http://127.0.0.1:5500', 'https://sandile44.github.io'],
+    origin: [
+      'http://localhost:5500',
+      'http://127.0.0.1:5500',
+      'https://sandile44.github.io',
+    ],
     credentials: true,
   })
 );
