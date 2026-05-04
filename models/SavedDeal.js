0fc5b50cff996393695ac38142900bf976857226
Sandile Thabede <thabethesandile44@gmail.com>
2026-05-04 12:26:46 +0200

    Update SavedDeal.js

--- models/SavedDeal.js
+++ models/SavedDeal.js
@@ -1,29 +1,34 @@
 import mongoose from "mongoose";
 
 const savedDealSchema = new mongoose.Schema(
-  {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true
-    },
-
-    type: {
-      type: String,
-      required: true
-    },
-
-    inputs: {
-      type: Object,
-      default: {}
-    },
-
-    results: {
-      type: Object,
-      default: {}
-    }
+{
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
   },
-  { timestamps: true }
+
+  type: {
+    type: String,
+    required: true
+  },
+
+  clientName: {                 // ✅ ADD THIS
+    type: String,
+    default: "Untitled Project"
+  },
+
+  inputs: {
+    type: Object,
+    default: {}
+  },
+
+  results: {
+    type: Object,
+    default: {}
+  }
+},
+{ timestamps: true }
 );
 
 export default mongoose.model("SavedDeal", savedDealSchema);
