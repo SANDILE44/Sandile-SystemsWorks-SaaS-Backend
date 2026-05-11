43ca4549454849278537f5f64ab0677eea07b7bb
Sandile Thabede <thabethesandile44@gmail.com>
2026-05-11 19:47:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(
     origin: [
       'http://localhost:5500',
       'http://127.0.0.1:5500',
-      'https://sandile44.github.io',
+      'https://sandile-systemsworks.vercel.app',
     ],
     credentials: true,
   })
